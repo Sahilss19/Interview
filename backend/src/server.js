cdf423f2231d35f6d7883036cc3766d70e913ed3
Sahil19 <sahilkulria9988@gmail.com>
2025-11-14 19:00:08 +0530

    env

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,19 @@
 // const express = require('express')
 
 import express from 'express';
+import {ENV} from './lib/env.js';
+
 const app = express();
+
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'succcccesss 123'});
 });
 
-app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+app.listen(ENV.PORT,() => {
+    console.log('Server is running on port:',ENV.PORT);
 });
