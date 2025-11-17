fe2eef9c420e982a96d4a12f7fc174297bbe3a6c
Sahil19 <sahilkulria9988@gmail.com>
2025-11-17 19:55:25 +0530

    auth and db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,39 @@
+import {Innjest} from 'inngest';
+import {connectDB} from './db.js';
+import User from '../models/User.js';
+// import {ENV} from './env.js';
+
+
+export const inngest = new Innjest({ id : 'INTERVIEW_APP'});
+
+const syncUser = inngest.createFunction(
+    { id : 'Sync User to DB'},
+    { event : 'clerk/user.created'},
+    async ({event}) => {
+        // connect to DB
+        await connectDB()
+
+        const {id , email_addresses , first_name , last_name , image_url} = event.data
+
+        const newUser = {
+            clerkId : id,
+            email : email_addresses[0]?.email_address,
+            name : `${first_name} ${last_name}`,
+            profileImage : image_url,
+        }
+
+       await User.create(newUser)
+    });
+
+
+const deleteUserFromDB = inngest.createFunction(
+    { id : 'delete User from DB'},
+    { event : 'clerk/user.deleted'},
+    async ({event}) => {
+        // connect to DB
+        await connectDB()
+
+        const {id} = event.data
+       await User.deletezOne({clerkId : id})
+    })
+export const functions = [syncUser , deleteUserFromDB];
