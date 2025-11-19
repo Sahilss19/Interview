25cfe6e724d6c6abdb5f8072335a3310097b7908
Sahil19 <sahilkulria9988@gmail.com>
2025-11-19 20:03:32 +0530

    fix port binding

--- backend/src/server.js
+++ backend/src/server.js
@@ -54,7 +54,8 @@ const start = async () => {
   try {
     await connectDB();
 
-    const PORT = process.env.PORT || ENV.PORT || 3000;
+    const PORT = process.env.PORT || 3000;
+
 
     app.listen(PORT, () => {
       console.log("Server running on:", PORT);
